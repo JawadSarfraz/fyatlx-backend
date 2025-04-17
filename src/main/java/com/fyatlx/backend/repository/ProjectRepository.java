0c42bd67cce1ab5813383d2498e2528bc96b3210
Jawwad Bin Sarfraz <jawadsarfraz95@gmail.com>
2025-04-18 01:40:58 +0200

    fix(repository): add missing @Repository annotation and imports to ProjectRepository

--- src/main/java/com/fyatlx/backend/repository/ProjectRepository.java
+++ src/main/java/com/fyatlx/backend/repository/ProjectRepository.java
@@ -2,6 +2,7 @@ package com.fyatlx.backend.repository;
 
 import com.fyatlx.backend.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
