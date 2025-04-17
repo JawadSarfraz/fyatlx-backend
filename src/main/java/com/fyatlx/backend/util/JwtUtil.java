54fb4f63f4c2c30d38cf875ed4585009067378f8
Jawwad Bin Sarfraz <jawadsarfraz95@gmail.com>
2025-04-17 12:53:20 +0200

    Fix missing import for StandardCharsets in JwtUtil

--- src/main/java/com/fyatlx/backend/util/JwtUtil.java
+++ src/main/java/com/fyatlx/backend/util/JwtUtil.java
@@ -6,12 +6,17 @@ import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
+import java.nio.charset.StandardCharsets;
+
 
 @Component
 public class JwtUtil {
 
     private final long expirationMs = 86400000; // 1 day
-    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private static final String SECRET = "zXyBz1aK94XMb93rKpdL6HJz2AfQeLMR09iZnchP7Xc="; // min 256-bit for HS256
+    private final Key key = Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+
+    // private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
     public String generateToken(String email) {
         return Jwts.builder()
