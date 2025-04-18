1923c93a8c3a87b7ad93b6771ef4fb853dcd8977
Jawwad Bin Sarfraz <jawadsarfraz95@gmail.com>
2025-04-18 02:25:10 +0200

    Add findByUser method to ProjectRepository to fetch user-specific project submissions

--- src/main/java/com/fyatlx/backend/repository/ProjectRepository.java
+++ src/main/java/com/fyatlx/backend/repository/ProjectRepository.java
@@ -1,9 +1,13 @@
 package com.fyatlx.backend.repository;
 
 import com.fyatlx.backend.entity.Project;
+import com.fyatlx.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+    List<Project> findByUser(User user);
 }
